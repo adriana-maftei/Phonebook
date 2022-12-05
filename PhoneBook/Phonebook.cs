971ac81c4eb175942aa795055785b4eca2944897
Adriana Maftei <115250887+adriana-maftei@users.noreply.github.com>
2022-12-05 18:41:27 +0200

    Upload

---
+++ PhoneBook/Phonebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneBook
+{
+    internal class Phonebook //responsible for managing contact list
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact) => _contacts.Add(contact);
+               // create a internal database to store contacts using list
+
+        public void ShowContactDetails(Contact contact)
+            => Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+
+        public void DisplayContactByNumber(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+                                      //search contact on contact list
+            if (contact == null) //to avoid null reference exception
+                Console.WriteLine("Contact not found");
+            else
+                ShowContactDetails(contact);
+        }
+
+        private void DisplaySearchedContact(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                ShowContactDetails(contact);
+            }
+        }
+
+        public void DisplayContactByName(string searchPhrase)
+                    // will display contacts that contain input letters
+        {
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+
+            if (matchingContacts == null)
+                Console.WriteLine("Contact not found");
+            else
+                DisplaySearchedContact(matchingContacts);
+        }
+
+        public void DisplayAllContacts() => DisplaySearchedContact(_contacts);
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("CONSOLE PHONEBOOK");
+            Console.WriteLine();
+            Console.WriteLine("Select operation:");
+            Console.WriteLine("Press 1 to Add contact");
+            Console.WriteLine("Press 2 to Display contact by number");
+            Console.WriteLine("Press 3 to Search contact by name");
+            Console.WriteLine("Press 4 to View all contact");
+            Console.WriteLine("Press x to Close application");
+            Console.WriteLine();
+
+            var userInput = Console.ReadLine();
+            var phoneBook = new Phonebook();
+
+            while (true) //the app will run forever until user wants to exit
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Write contact name:");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Write contact number:");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter a number to search:");
+                        var searchNumber = Console.ReadLine();
+                        phoneBook.DisplayContactByNumber(searchNumber);
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Enter a name to search:");
+                        var searchName = Console.ReadLine();
+                        phoneBook.DisplayContactByName(searchName);
+                        break;
+
+                    case "4":
+                        phoneBook.DisplayAllContacts();
+                        break;
+
+                    case "x":
+                        return;
+
+                    default:
+                        Console.WriteLine("Please choose a valid operation");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Select operation:");
+                userInput = Console.ReadLine();
+                //will prevent user to stuck on the first selected case forever
+            }
+        }
+    }
+}
